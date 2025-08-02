822d6ee84bfcf7a71d87fed45123d44ad27b3cbe
carlfreeman <carlfreeman875@gmail.com>
2025-08-02 12:20:11 +0300

    deploy

--- server/server.js
+++ server/server.js
@@ -83,6 +83,9 @@ if (process.env.NODE_ENV === 'production') {
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, '../public/index.html'));
     });
+if (process.env.NODE_ENV !== 'production') {
+    app.use(express.static(path.join(__dirname, '../public')));
+}
 }
 
 module.exports = app;
